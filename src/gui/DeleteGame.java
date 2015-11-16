5edb469d5a4d9674eeaffd17f201c02c16db2c57
li.jacky7@gmail.com <SNSDjacky4ever>
2015-11-16 11:29:13 +0100

    GUI - set background, color, bounds for deletegame eller creategame

--- src/gui/DeleteGame.java
+++ src/gui/DeleteGame.java
@@ -1,18 +1,22 @@
 package gui;
 
 import javax.swing.JPanel;
+import java.awt.Color;
 
 /**
  * Created by Jacky on 16/11/15.
  */
-public class DeleteGame {
+public class DeleteGame extends JPanel {
 
     /**
      * Create the panel
      */
 
     public DeleteGame(){
-
+        setForeground(Color.GRAY);
+        setBackground(Color.LIGHT_GRAY);
+        setBounds(100, 100, 668, 395);
+        setLayout(null);
     }
 
 }
