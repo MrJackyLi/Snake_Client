f1628f7aa957172acaf94e76c7c7cd95305840a8
li.jacky7@gmail.com <SNSDjacky4ever>
2015-11-19 09:29:36 +0100

    GUI - still trying to solve gui issue, to bond the gui classes together.. did outcommet the old statemen and trying with a new one. pt3

--- src/sdk/Logic.java
+++ src/sdk/Logic.java
@@ -22,63 +22,10 @@ public class Logic {
         public void run(){
             screen.getLogin().actionPerformedLogin(
                     new LoginActionListener());
-            screen.getMenu().addACList(
-                    new MenuActionListener());
-
-            screen.show(screen.LOGIN);
+            //screen.getMenu().addACList(
+                   // new MenuActionListener());
+            screen.show(Screen.LOGIN);
         }
-
-
-        /*public static void login(String username, String password){
-
-            ServerConnection serverConnection = new ServerConnection();
-
-            User user = new User();
-            user.setPassword(password);
-            user.setUsername(username);
-
-            String json = new Gson().toJson(user);
-
-            serverConnection.post(json, "login/");
-
-
-        }*/
-        /*public static void createUser(User user){
-
-        }
-        public static void deleteUser(int userId){
-
-        }
-        public static void getUser(int userId){
-
-        }
-        public static void getGame(int gameId){
-
-        }
-        public static void joinGame(int gameId, User opponent, String controls){
-
-        }
-        public static void startGame(int gameId){
-
-        }
-        public static void createGame(String name, int status){
-
-            ServerConnection serverConnection = new ServerConnection();
-
-            Game game = new Game();
-            game.setName(name);
-            //game.setHost();
-            game.setStatus(status);
-
-            String json = new Gson().toJson(game);
-
-            serverConnection.post(json, "create");
-
-        }
-        public static void deleteGame(int gameId){
-
-        }
-*/
 
     private boolean isEmpty(String text){
         //trim sørger for at der ikke er tomme spaces
@@ -94,32 +41,15 @@ public class Logic {
     private class LoginActionListener implements ActionListener{
 
         public void actionPerformed(ActionEvent e){
-         //   String actCom = e.getActionCommand();
-         //   if (actCom.equals("Login")) {
-                screen.show(screen.MENU);
 
-                /*String usernameField = screen.getLogin().getTxtUsername().getText();
-                String passwordField = screen.getLogin().getTxtTypePassword().getText();
-                screen.getLogin().getTxtUsername().setText("");
-                screen.getLogin().getTxtTypePassword().setText("");
-                    //Checks if username and pass field is filled
-                if (isEmpty(usernameField) || isEmpty(passwordField)) {
-                    //Errormsg in label in window
-                    screen.getLogin().setErrorMessage("Please type username and password");
-                } else {
-                    screen.getLogin().setErrorMessage("Wrong username or password");
-                    //Checking login
-                    if (null)
-                        screen.show.(screen.MENU);
-                }
-*/
+                screen.show(Screen.MENU);
 
                 }
 
             }
         }
 
-    
+
 
 
 
